8acc266dde8aae1681f40e2ede5c3fd2a8892cec
StanTheMan7 <sts.graur@gmail.com>
2022-08-21 20:45:41 +0200

    file/folders structure created

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Portfolio.css'
+
+
+export default function Portfolio() {
+  return (
+    <div>Portfolio</div>
+  )
+}
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Testimonials.css'
+
+
+export default function Testimonials() {
+  return (
+    <div>Testimonials</div>
+  )
+}
