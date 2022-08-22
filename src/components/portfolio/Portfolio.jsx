4f95eb2bb2bee506f12e9168d86b29b5bf15417b
StanTheMan7 <sts.graur@gmail.com>
2022-08-22 18:06:11 +0200

    Generating Porfolio Items dynamicly from array using map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,52 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const portfolioData = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Fitness Booking",
+    github: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final",
+    demo: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Calculator",
+    github: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final",
+    demo: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Todo List",
+    github: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final",
+    demo: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Booking Agency Design",
+    github: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final",
+    demo: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Restaurant Site Design",
+    github: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final",
+    demo: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Dram Kit Keybord Interaction",
+    github: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final",
+    demo: "https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+  }
+];
+
+
 
 export default function Portfolio() {
   return (
@@ -14,113 +60,29 @@ export default function Portfolio() {
       <h5 className="text-light">My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              className="btn "
-              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
-            >
-              GitHub
-            </a>
-            <a className="btn btn-primary" href="">
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              className="btn "
-              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
-            >
-              GitHub
-            </a>
-            <a className="btn btn-primary" href="">
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              className="btn "
-              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
-            >
-              GitHub
-            </a>
-            <a className="btn btn-primary" href="">
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              className="btn "
-              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
-            >
-              GitHub
-            </a>
-            <a className="btn btn-primary" href="">
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              className="btn "
-              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
-            >
-              GitHub
-            </a>
-            <a className="btn btn-primary" href="">
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              className="btn "
-              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
-            >
-              GitHub
-            </a>
-            <a className="btn btn-primary" href="">
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {
+          portfolioData.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title}/>
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a
+                    className="btn "
+                    href={github}
+                  >
+                    GitHub
+                  </a>
+                  <a className="btn btn-primary" href={demo}>
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            );
+          })
+        }
       </div>
     </section>
   );
