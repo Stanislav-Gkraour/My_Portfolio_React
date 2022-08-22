cc958be77e8879d5cc52bcfa452c4e5cc339190e
StanTheMan7 <sts.graur@gmail.com>
2022-08-22 16:49:54 +0200

    Portfolio jsx/css components, responive design on tabs/phones

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 
 export default function Portfolio() {
@@ -9,9 +15,111 @@ export default function Portfolio() {
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final">Fitness</a>
+          <div className="portfolio__item-cta">
+            <a
+              className="btn "
+              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+            >
+              GitHub
+            </a>
+            <a className="btn btn-primary" href="">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              className="btn "
+              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+            >
+              GitHub
+            </a>
+            <a className="btn btn-primary" href="">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              className="btn "
+              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+            >
+              GitHub
+            </a>
+            <a className="btn btn-primary" href="">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              className="btn "
+              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+            >
+              GitHub
+            </a>
+            <a className="btn btn-primary" href="">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              className="btn "
+              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+            >
+              GitHub
+            </a>
+            <a className="btn btn-primary" href="">
+              Live Demo
+            </a>
+          </div>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a
+              className="btn "
+              href="https://github.com/Stanislav-Gkraour/Laravel-Project-Final"
+            >
+              GitHub
+            </a>
+            <a className="btn btn-primary" href="">
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
